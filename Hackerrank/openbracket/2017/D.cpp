bd9a77363fbeb7994af22fcc6dc7333004133a5b
Rares Cristian <raresc2105@gmail.com>
2017-08-09 11:35:17 -0400

    Verdict: AC. Solve by preprocessing previous smallest value of temp[]

--- Hackerrank/openbracket/2017/D.cpp
+++ Hackerrank/openbracket/2017/D.cpp
@@ -9,117 +9,127 @@ typedef long long int lli;
 
 #define pb push_back
 #define mp make_pair
-
 #define MAX 2005
-// int grid[MAX][MAXM], amnt[MAX][MAXM];
+
+int N, M; 
+int temp[MAX * MAX];
+int prev_[MAX * MAX], dp[MAX * MAX];
+int smallest_prev[MAX * MAX];
+vector<vector<int> > grid;
+vector<vector<int> > amnt;
+
+inline void build_tight(int i, int k) {
+	int cur = amnt[i][k];
+	int tmp = cur;            
+	int b = k - cur + 1;
+	    
+	if (b >= 0 && grid[i][b] < grid[i - 1][k]) {
+		int minim = b, maxim = k;
+		int best = MAX * MAX;
+		while (minim <= maxim) {
+			int mid = (minim + maxim) / 2;
+			if (grid[i][mid] >= grid[i - 1][k]) {
+				maxim = mid - 1;
+				best = min(best, mid);
+			} else {
+				minim = mid + 1;
+			}
+		}
+		if (best == MAX*MAX) cur = 0;
+		else
+			cur = k - best + 1;
+	}
+    temp[i] = cur;
+}
+
+inline void build_stack(int k) {
+	stack<pair<int, int> > st;
+	for (int j = N - 1; j >= 0; j--) {
+		while (!st.empty() && st.top().first >= temp[j]) {
+			smallest_prev[st.top().second] = j;
+			st.pop();
+		}
+		st.push(mp(temp[j], j));
+	}
+	while (!st.empty()) {
+		smallest_prev[st.top().second] = -1; // have none that are smaller
+		st.pop();
+	}
+}
 
 int main() {
 
-    clock_t begin = clock();
-    
-    int N, M; cin >> N >> M;
-    int NN = N, MM = M;
-    int grid[N][M], amnt[N][M];
-    //if (N > M) swap(NN, MM);
-    
+    cin >> N >> M;
+    grid.resize(N, vector<int>(M, 0));
+    amnt.resize(N, vector<int>(M, 0));
     for (int i = 0; i < N; i++) {
-    for (int k = 0; k < M; k++) {
-	    int a; scanf("%d", &a);
-	    //	    if (N > M) grid[k][i] = a;
-	    grid[i][k] = a;
-	}
+    	for (int k = 0; k < M; k++) {
+	    	scanf("%d", &grid[i][k]);
+		}
     }
 
-    //    N = NN, M = MM;
-    int prev_[N], dp[N];
-    int temp[N];
-
     for (int i = 0; i < N; i++) {
-	int k = 1;
-	int start_ = 0;
+		int k = 1;
+		int start_ = 0;
 	
-	while (k <= M) {
-	    while (k < M && grid[i][k] >= grid[i][k - 1])
-		k++;
-	    for (int j = start_; j < k; j++) {
-		amnt[i][j] = j - start_ + 1;
-	    }
-	    start_ = k;
-	    k++;
-	}
+		while (k <= M) {
+	    	while (k < M && grid[i][k] >= grid[i][k - 1])
+				k++;
+	    	for (int j = start_; j < k; j++) {
+				amnt[i][j] = j - start_ + 1;
+	    	}
+	    	start_ = k;
+	    	k++;
+		}
 	
     }
 
     lli ans = 0;
     for (int k = 0; k < M; k++) {
-	ans += amnt[0][k];
+		ans += amnt[0][k];
     }
-    
+
     for (int k = 0; k < M; k++) {
-	temp[0] = dp[0] = amnt[0][k];
-	prev_[0] = 0;
-	for (int i = 1; i < N; i++) {
-	    dp[i] = 0;
-	    int cur = amnt[i][k];
-	    int tmp = cur;
-	    int b = k - cur + 1;
-	    
-	    if (b >= 0 && grid[i][b] < grid[i - 1][k]) {
-		int minim = b, maxim = k;
-		int best = MAX * MAX;
-		while (minim <= maxim) {
-		    int mid = (minim + maxim) / 2;
-		    if (grid[i][mid] >= grid[i - 1][k]) {
-			maxim = mid - 1;
-			best = min(best, mid);
-		    } else {
-			minim = mid + 1;
+		temp[0] = dp[0] = amnt[0][k];
+		prev_[0] = 0;
+		
+		for (int i = 1; i < N; i++)
+			build_tight(i, k);
+		build_stack(k);
+
+		for (int i = 1; i < N; i++) {
+		    dp[i] = 0;
+	    	int cur = amnt[i][k];
+	    	int tmp = cur;
+            cur = temp[i];
+
+		    if (cur == 0) {
+				dp[i] = amnt[i][k];
+				ans += dp[i];
+				prev_[i] = i;
+				continue;
 		    }
-		}
-		if (best == MAX*MAX) cur = 0;
-		else
-		    cur = k - best + 1;
-	    }
 
-	    if (cur == 0) {
-		dp[i] = amnt[i][k];
-		ans += dp[i];
-		prev_[i] = i;
-		continue;
-	    }
+		    int indx = smallest_prev[i];
+		    if (indx < prev_[i - 1]) indx = -1;
+		    else if (amnt[indx][k] > cur) {
+			    dp[i] += cur - amnt[indx][k];
+		    }
 
-	    int indx = -1;
-	    for (int j = i - 1; j >= prev_[i - 1]; j--) {
-		if (amnt[j][k] <= cur) {
-		    indx = j;
-		    break;
+		    if (indx == -1) {
+				dp[i] = cur * (i - prev_[i - 1] + 1);
+		    } else {
+				dp[i] += cur * (i - indx);
+				dp[i] += dp[indx];
+		    }
+		    
+		    dp[i] += tmp - cur;
+		    ans += dp[i];
+		    prev_[i] = prev_[i - 1];
 		}
-		if (temp[j] <= cur) {
-		    indx = j;
-		    dp[i] += cur - amnt[j][k];
-		    break;
-		}
-	    }
-
-	    if (indx == -1) {
-		dp[i] = cur * (i - prev_[i - 1] + 1);
-	    } else {
-		dp[i] += cur * (i - indx);
-		dp[i] += dp[indx];
-	    }
-	    
-	    dp[i] += tmp - cur;
-	    temp[i] = cur;
-	    ans += dp[i];
-	    prev_[i] = prev_[i - 1];
-	}
     }
     
     cout << ans << "\n";
-
-    clock_t end = clock();
-    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    //cout << elapsed_secs << "\n";
     
     return 0;
 }
