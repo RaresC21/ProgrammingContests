8d842f77be507576e64194135a9f4953d96183d0
Rares Cristian <raresc2105@gmail.com>
2017-08-16 20:28:50 -0400

    correct factor when input has multiplicity of a divisor

--- ProjectEuler/p3-largest-prime.hs
+++ ProjectEuler/p3-largest-prime.hs
@@ -4,17 +4,23 @@ What is the largest prime factor of the number 600851475143 ?
 -}
 
 input = 600851475143
-
-factor :: Integer -> Integer -> [Integer] -> Integer
+  
+factor :: Int -> Int -> [Int] -> Int
 factor value num primes
-    | (value == num)        = num
-    | is_prime num primes   = if value `mod` num == 0 
-                                then factor (quot value num) (num + 1) (num:primes)
-                                else factor (value) (num + 1) (num:primes)
-    | otherwise             = factor (value) (num + 1) (primes)
+  | (value == num)        = num
+  | is_prime num primes   = if value `mod` num == 0 
+                            then factor (remove_factor value num) (num + 1) (num:primes)
+                            else factor (value) (num + 1) (num:primes)
+  | otherwise             = factor (value) (num + 1) (primes)
+
+remove_factor :: Int -> Int -> Int
+remove_factor value num =
+  if value `mod` num == 0
+  then remove_factor (quot value num) (num)
+  else value
 
 is_prime x primes = all (\p -> x `mod` p > 0) (try x)
-    where 
-        try x = takeWhile (\p -> p*p <= x) primes
+  where 
+    try x = takeWhile (\p -> p*p <= x) primes
 
 main = print (factor input 2 [])
