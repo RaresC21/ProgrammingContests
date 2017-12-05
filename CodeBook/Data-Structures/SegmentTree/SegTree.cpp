2b11a598481e5ea95eae7509b7b7f7ce96ebae75
Rares Cristian <raresc2105@gmail.com>
2017-12-05 11:00:20 -0500

    use templates to generalize nodes in SegTree

--- CodeBook/Data-Structures/SegmentTree/SegTree.cpp
+++ CodeBook/Data-Structures/SegmentTree/SegTree.cpp
@@ -1,34 +1,42 @@
 const int MAX = 100005;
 
+template<class T>
 class SegTree {
 private:
 
     int N;
-    std::vector<int>& A;
-    int tree[2 * MAX], lazy[2 * MAX];
-    int (*op) (int a, int b);
+    std::vector<T>& A;
+    T tree[4 * MAX];
 
     void build(int node, int start, int end);
-    void update(int node, int start, int end, int indx, int val);
-    int query(int node, int start, int end, int l, int r, int default_val);
+    void update(int node, int start, int end, int indx, T val);
+    T query(int node, int start, int end, int l, int r, T default_val);
+    T merge (const T& a, const T& b);
 
 public:
 
-    SegTree(std::vector<int>& a, int (*op)(int a, int b)) : A(a), op(op) {
+    SegTree(std::vector<T>& a) : A(a) {
         N = A.size();
         build(1, 0, N - 1);
     }
 
-    void update(int indx, int val) {
+    void update(int indx, T val) {
         update(1, 0, N - 1, indx, val);
     }
 
-    int query(int l, int r, int default_val) {
+    T query(int l, int r, T default_val) {
         return query(1, 0, N - 1, l, r, default_val);
     }
 };
 
-void SegTree::build(int node, int start, int end) {
+template<class T> // currently supports range minimum query.
+T SegTree<T>::merge(const T& a,  const T& b) {
+    if (a < b) return a;
+    return b;
+}
+
+template<class T>
+void SegTree<T>::build(int node, int start, int end) {
     if (start == end) { // leaf node
         tree[node] = A[start];
         return;
@@ -38,13 +46,14 @@ void SegTree::build(int node, int start, int end) {
     build(2 * node, start, mid);
     build(2 * node + 1, mid + 1, end);
 
-    tree[node] = (*op)(tree[2 * node], tree[2 * node + 1]);
+    tree[node] = (*merge)(tree[2 * node], tree[2 * node + 1]);
 }
 
-void SegTree::update(int node, int start, int end, int indx, int val) {
+template<class T>
+void SegTree<T>::update(int node, int start, int end, int indx, T val) {
     if (start == end) {
-        A[indx] = (*op)(A[indx], val);
-        tree[node] = (*op)(tree[node], val);
+        A[indx] = (*merge)(A[indx], val);
+        tree[node] = (*merge)(tree[node], val);
         return;
     }
 
@@ -54,10 +63,11 @@ void SegTree::update(int node, int start, int end, int indx, int val) {
     else
         update(2 * node + 1, mid + 1, end, indx, val);
 
-    tree[node] = (*op)(tree[node * 2], tree[node * 2 + 1]);
+    tree[node] = (*merge)(tree[node * 2], tree[node * 2 + 1]);
 }
 
-int SegTree::query(int node, int start, int end, int l, int r, int default_val) {
+template<class T>
+T SegTree<T>::query(int node, int start, int end, int l, int r, T default_val) {
     if (r < start || l > end)
         return default_val;
 
@@ -66,8 +76,8 @@ int SegTree::query(int node, int start, int end, int l, int r, int default_val) 
 
     //partially in range, partially out.
     int mid = (start + end) / 2;
-    int p1 = query(node * 2, start, mid, l, r, default_val);
-    int p2 = query(node * 2 + 1, mid + 1, end, l, r, default_val);
+    T p1 = query(node * 2, start, mid, l, r, default_val);
+    T   p2 = query(node * 2 + 1, mid + 1, end, l, r, default_val);
 
-    return (*op)(p1, p2);
+    return (*merge)(p1, p2);
 }
